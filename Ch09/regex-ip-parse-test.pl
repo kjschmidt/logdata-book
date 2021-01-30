2de5c25140f7382bee0ad4ac907b7841e5f3bdde
kjschmidt <kjschmidt@gmail.com>
2021-01-30 16:58:00 -0600

    Update regex-ip-parse-test.pl

--- Ch09/regex-ip-parse-test.pl
+++ Ch09/regex-ip-parse-test.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
-
+#
+# Run with: # time script.pl
+#
 my $text = “I would like to get the following IP address: 10.0.0.2”;
 for(my $a=0; $a<100000; $a++) {
     my($IP) = $text =∼ m/: (.*)$/;
