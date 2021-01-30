da7f3e95bf42da5a3824811f10a05e04d1481f70
kjschmidt <kjschmidt@gmail.com>
2021-01-30 16:53:26 -0600

    Create snare-parse.pl

---
+++ Ch09/snare-parse.pl
@@ -0,0 +1,3 @@
+my($cruft,$criticality,$sourceName1,$count,$dateTime,$eventId,$sourceName2,
+$user,$sidType,$eventLogType,$computerName,$categoryString,$dataString,
+$expandedString,$md5) = split(/\t/,$message);
