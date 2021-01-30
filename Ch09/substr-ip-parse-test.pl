4382502a9bccfadc6b55423cc4d09414ad4a6441
kjschmidt <kjschmidt@gmail.com>
2021-01-30 16:57:46 -0600

    Create substr-ip-parse-test.pl

---
+++ Ch09/substr-ip-parse-test.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+#
+# Run with: # time script.pl
+#
+my $text = “I would like to get the following IP address: 10.0.0.2”;
+for(my $a=0; $a<100000; $a++) {
+    my $IP = substr $text, 46;
+}
