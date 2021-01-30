8c37bb5a4178c245fdb2f23c747ad6ba03239068
kjschmidt <kjschmidt@gmail.com>
2021-01-30 16:55:03 -0600

    Update snare-parse.pl

--- Ch09/snare-parse.pl
+++ Ch09/snare-parse.pl
@@ -1,3 +1,1 @@
-my($cruft,$criticality,$sourceName1,$count,$dateTime,$eventId,$sourceName2,
-$user,$sidType,$eventLogType,$computerName,$categoryString,$dataString,
-$expandedString,$md5) = split(/\t/,$message);
+my($cruft,$criticality,$sourceName1,$count,$dateTime,$eventId,$sourceName2, $user,$sidType,$eventLogType,$computerName,$categoryString,$dataString,$expandedString,$md5) = split(/\t/,$message);
